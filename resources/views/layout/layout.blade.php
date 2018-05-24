6d68889bb348adbb7af58c6f580279fca00eecd4
Jelle <jlbosch@hotmail.nl>
2018-05-24 13:42:59 +0200

    Changes
    CSS files added +link
    Layout changed

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,16 +5,10 @@
         <title>@yield('title')</title>
     </head>
     <body>
-        <header class="header">
-            @include('include.header')
-        </header>
 
-        <div class="container">
-            <h1>@yield('content')</h1>
-        </div>
+        @include('include.header')
+        @yield('content')
+        @include('include.footer')
 
-        <footer class="footer">
-            @include('include.footer')
-        </footer>
     </body>
 </html>
