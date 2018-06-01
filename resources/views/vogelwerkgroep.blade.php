658afe64b94fe44176f12716b3c4e80cdacc569a
Guido P Vosselman <pgv@live.nl>
2018-06-01 11:28:18 +0200

    Auto stash before merge of "View" and "origin/View"

--- resources/views/vogelwerkgroep.blade.php
+++ resources/views/vogelwerkgroep.blade.php
@@ -7,9 +7,30 @@ Vogelwerkgroep
 @section('content')
     <section style="width: calc(100% - 250px);float: right">
         <h1>@yield('title')</h1>
-        <p>
-            {{}}
-        </p>
+        <div class="container">
+            <div class="col-md-4">
+                <p>
+                    kut njelle
+                </p>
+            </div>
+            <div class="col-md-6">
+                <p>
+                    Mei 2018
+
+                    In maart zagen we nog wel wat grutto's in de weilanden, maar intussen vragen we ons af of er überhaupt zijn
+                    gebleven om te broeden. Kieviten en scholeksters zien we gelukkig nog wel, al is het dan nog steeds de vraag
+                    of ze erin slagen kuikens groot te brengen onder de huidige slechte omstandigheden in Cronesteyn.
+
+                    De boomklever die in maart nog in het park rondhing, is sindsdien ook niet meer gesignaleerd.
+                    Het zou ook de eerste boomklever zijn geweest die bij ons kwam broeden.
+                    Met de ijsvogels ging het de laatste jaren lekker in Cronesteyn, maar dit jaar lijkt het erop dat ze het
+                    hebben afgelegd tegen de herhaalde vorstperiodes.
+                </p>
+            </div>
+        </div>
+
+
+
     </section>
 @endsection
 
