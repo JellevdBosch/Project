e8d464ccf74f535b3f5acbbdd36a4323398965d3
Jelle <jlbosch@hotmail.nl>
2018-06-01 13:13:33 +0200

    error fix, index css, landing added

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -1,36 +1,30 @@
-<?php use Illuminate\Support\Facades\Route;?>
 <section id="sidebar">
     <div id="sidebar-wrapper">
-        <?php
-
-        if (Route::is(‘index’)) {
-            echo '<article class="sidebar-article">
-                    <div class="sidebar-article-title">
-                        <h5 class="">XD</h5>
-                    </div>
-                    <ul class="sidebar-list">
-                        <li class="sidebar-list-item">
-                            <button>Test</button>
-                        </li>
-                        <li class="sidebar-list-item">
-                            <button>Test</button>
-                        </li>
-                    </ul>
-                </article>
-                <article class="sidebar-article">
-                    <div class="sidebar-article-title">
-                        <h5 class="">Test</h5>
-                    </div>
-                    <ul class="sidebar-list">
-                        <li class="sidebar-list-item">
-                            <button>Test</button>
-                        </li>
-                        <li class="sidebar-list-item">
-                            <button>Test</button>
-                        </li>
-                    </ul>
-                </article>';
-        }
-        ?>
+        <article class="sidebar-article">
+            <div class="sidebar-article-title">
+                <h5 class="">Test</h5>
+            </div>
+            <ul class="sidebar-list">
+                <li class="sidebar-list-item">
+                    <button>Test</button>
+                </li>
+                <li class="sidebar-list-item">
+                    <button>Test</button>
+                </li>
+            </ul>
+        </article>
+        <article class="sidebar-article">
+            <div class="sidebar-article-title">
+                <h5 class="">Test</h5>
+            </div>
+            <ul class="sidebar-list">
+                <li class="sidebar-list-item">
+                    <button>Test</button>
+                </li>
+                <li class="sidebar-list-item">
+                    <button>Test</button>
+                </li>
+            </ul>
+        </article>
     </div>
 </section>
