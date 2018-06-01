bdf6245de8bea812ca552cffb0192fa2c35592f7
Unknown <00124626@student.rocleiden.nl>
2018-06-01 13:10:01 +0200

    Update
    Test page with form to insert record into database

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Vogelwerkgroep;
+use Request;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -11,40 +12,65 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function index() {
+    public function index()
+    {
 
         return view('index');
 
     }
 
-    public function activiteiten() {
+    public function activiteiten()
+    {
 
         return view('activiteiten');
 
     }
 
-    public function parkvereniging() {
+    public function parkvereniging()
+    {
 
         return view('parkvereniging');
 
     }
 
-    public function vogelwerkgroep() {
+    public function vogelwerkgroep()
+    {
 
         return view('vogelwerkgroep');
 
     }
 
-    public function fotos() {
+    public function fotos()
+    {
 
         return view('fotos');
 
     }
 
-    public function contact() {
+    public function contact()
+    {
 
         return view('contact');
 
+    }
+
+    public function test()
+    {
+        return view('test/test');
+    }
+
+    public function create()
+    {
+        $user = new Vogelwerkgroep;
+
+        $user->title = Request::get('title');
+        $user->text = Request::get('text');
+        $user->save();
+
+//        Session::flash('message', "Special message goes here");
+//        return Redirect::back();
+
+        return view('test/test');
     }
 }
 
