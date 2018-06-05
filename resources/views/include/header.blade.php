2cf8cb7039b67d639991f2a4150ca0f9e1b8d1b5
Jelle <jlbosch@hotmail.nl>
2018-06-05 11:42:31 +0200

    shit

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,5 +1,26 @@
 <header id="header">
     <section id="header-wrapper">
+        <div id="main-nav-dropdown">
+
+                <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle " data-toggle="dropdown"><span class="glyphicon glyphicon-user pull-right"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Account Settings <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">User stats <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Messages <span class="badge pull-right"> 42 </span></a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Favourites Snippets <span class="glyphicon glyphicon-heart pull-right"></span></a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Sign Out <span class="glyphicon glyphicon-log-out pull-right"></span></a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+        </div>
+
         <div id="main-logo">
             <img src="{{asset('images/logo.png')}}" alt="Polderpark Cronesteyn Logo">
         </div>
@@ -7,25 +28,30 @@
             <nav id="main-nav-wrapper">
                 <ul>
                     <li>
-                        <button>Home</button>
+                        <a>Home</a>
                     </li>
                     <li>
-                        <button>Home</button>
+                        <a>Activiteiten</a>
                     </li>
                     <li>
-                        <button>Home</button>
+                        <a>Parkvereniging</a>
                     </li>
                     <li>
-                        <button>Home</button>
+                        <a>Vogelwerkgroep</a>
                     </li>
                     <li>
-                        <button>Home</button>
+                        <a>Foto's</a>
                     </li>
                     <li>
-                        <button>Home</button>
+                        <a>Contact</a>
                     </li>
                 </ul>
             </nav>
+        </div>
+        <div id="login-info">
+            <p>
+                <b>Welkom Jelle</b>
+            </p>
         </div>
     </section>
 </header>
