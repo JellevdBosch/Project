45ed8bb39a198595295b31ed49eaebf4f89e2cf5
Unknown <00124626@student.rocleiden.nl>
2018-06-07 12:31:02 +0200

    Header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,16 @@
 
 @section('landing')
     <section id="landing">
-        
+        <section class="grid-landing">
+            <div class="landing-item"><button type="button" class="btn btn-success">
+                    Success
+                </button>
+            </div>
+            <div class="landing-item image">
+                <div class="image"></div>
+                {{--<img src="https://www.wallpaperup.com/uploads/wallpapers/2014/05/23/356373/21df65eddd49cf3c8a46bc62c8149a53-1000.jpg">--}}
+            </div>
+        </section>
     </section>
 @endsection
 
