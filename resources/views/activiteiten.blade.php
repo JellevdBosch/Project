dc350c99f27fe5c8f7c765fd2b154df01f174173
Jelle <jlbosch@hotmail.nl>
2018-06-26 10:00:31 +0200

    STYLE AND HTML CHANGED: NOW RESPONSIVE AND LOOKS BETTER

--- resources/views/activiteiten.blade.php
+++ resources/views/activiteiten.blade.php
@@ -7,13 +7,22 @@
 @section('content')
     <section id="content">
         <div id="page wrapper">
-            @if($activities->count())
-                @foreach($activities as $activity)
-                    <h3>{{ $activity->title }}</h3>
-                    <p>{{ $activity->activity }}</p>
-                    <small>{{ $activity->date }}</small>
-                @endforeach
-            @endif
+            <div class="container">
+                <section class="row">
+                    <h2 class="col-md-12">Activiteiten in Polderpark Cronesteyn</h2>
+                    <h5 class="col-md-12">Programma van 2018</h5>
+                    <h6 class="col-md-12">Alle activiteiten starten bij de tuin van de smid, tenzij anders aangegeven.</h6>
+                </section>
+                <section class="row">
+                    @if($activities->count())
+                        @foreach($activities as $activity)
+                            <h3 class="col-md-12">{{ $activity->title }}</h3>
+                            <p class="col-md-12">{{ $activity->activity }}</p>
+                            <small class="col-md-12">{{ $activity->date }}</small>
+                        @endforeach
+                    @endif
+                </section>
+            </div>
         </div>
     </section>
 @endsection
--- resources/views/admin/activiteit.blade.php
+++ resources/views/admin/activiteit.blade.php
@@ -26,9 +26,9 @@
         </div>
     @endif
     <div style="margin-bottom: 120px;" class="container">
-        <div class="col-lg-2">
+        <div class="col-lg-2 col-sm-12 col-xs-12 col-md-2">
             <nav class="nav-sidebar">
-                <ul class="nav tabs">
+                <ul class="nav text-center tabs">
                     <li class="active">
                         <a href="{{ url('admin/image-gallery') }}">Afbeeldingen</a>
                     </li>
@@ -36,12 +36,8 @@
                 </ul>
             </nav>
         </div>
-        <div class="col-lg-10">
-            <h3>Activiteit toevoegen
-                <span style="float:right">
-                    <a style="color: #5d963f" href="{{ url('activiteiten') }}">Naar Activiteiten</a>
-                </span>
-            </h3>
+        <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12 text-center">
+            <h3 style="margin-bottom: 30px">Activiteit toevoegen</h3>
             <form action="{{ url('admin/activiteiten') }}" class="form-image-upload" method="POST"
                 enctype="multipart/form-data">
                 {!! csrf_field() !!}
@@ -55,7 +51,7 @@
                     </div>
                 @endif
                 <div class="row">
-                    <div class="form-group col-md-4">
+                    <div class="form-group col-lg-4 col-md-4 col-sm-12 col-xs-12">
                         <label for="title">Titel:
                             <input autocomplete="off" type="text" class="form-control" name="title">
                         </label>
@@ -63,12 +59,12 @@
                             <span class="text-danger">{{ $errors->first('name') }}</span>
                         @endif
                     </div>
-                    <div class="form-group col-md-4">
+                    <div class="form-group col-lg-4 col-md-4 col-sm-12 col-xs-12">
                         <label for="activity">Activiteit:
                             <textarea type="text" class="form-control" name="activity"></textarea>
                         </label>
                     </div>
-                    <div class="form-group col-md-4">
+                    <div class="form-group col-lg-4 col-md-4 col-sm-12 col-xs-12">
                         <label for="date">Datum:
                             <input autocomplete="off" type="date" class="form-control" name="date">
                         </label>
@@ -81,23 +77,24 @@
                     </div>
                 </div>
             </form>
-            <div class="row">
-                <h3 style="color: #5d963f">Delete Activities:</h3>
+            <div class="row text-center">
+                <h3 style="color: #5d963f">Verwijder Activiteiten:</h3>
                 <div class='list-group gallery'>
                     @if(!empty($activities) && $activities->count())
                         @foreach($activities as $activity)
-                            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3' style="border: 0.5px solid #5d963f; padding: 10px">
+                            <div class='col-sm-11 col-xs-11 col-md-3 col-lg-3' style="margin:15px;max-height:250px;height:250px;overflow-y:hidden;border: 0.5px solid #5d963f; padding: 10px">
                                 <form action="{{ url('admin/activiteiten/',$activity->id) }}" method="POST">
                                     <input type="hidden" name="_method" value="delete">
                                     {!! csrf_field() !!}
-                                    <button type="submit" style="background: none;" class="close-icon btn">
+                                    <button type="submit" style="background: none;padding: 0" class="close-icon btn">
                                         <i style="color: #5d963f; font-size: 18px;" class="glyphicon glyphicon-remove"></i>
                                     </button>
                                 </form>
-                                <div class='text-center'>
-                                    <small class='text-muted'>{{ $activity->title }}</small><br>
-                                    <small class='text-muted'>{{ $activity->activity }}</small><br>
-                                    <small class='text-muted'>{{ $activity->date }}</small><br>
+                                <div class='text-left' style="max-width: 100%; overflow-x: hidden; overflow-y: hidden">
+                                    <h6><b class='text-muted'>{{ $activity->title }}</b></h6>
+                                    <p style="max-width: inherit;height: 125px;max-height: 125px; overflow-y: scroll;
+                                    overflow-x: hidden;overflow-wrap: break-word;" class='text-muted'>{{ $activity->activity }}</p>
+                                    <small style="padding: 10px" class='text-muted'>{{ $activity->date }}</small><br>
                                 </div>
                             </div>
                         @endforeach
