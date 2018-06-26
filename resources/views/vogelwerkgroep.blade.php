5e20d26241c157012610fbd17a6547927274b64d
Jasper <00124626@student.rocleiden.nl>
2018-06-26 11:21:49 +0200

    Added vogelwerkgroep

--- resources/views/vogelwerkgroep.blade.php
+++ resources/views/vogelwerkgroep.blade.php
@@ -6,27 +6,116 @@
 
 @section('content')
     <section>
-        <div class="col-md-6">
-            <h2>@yield('title')</h2>
-            <p>
-                In maart zagen we nog wel wat grutto's in de weilanden, maar intussen vragen we ons af of er
-                überhaupt zijn
-                gebleven om te broeden. Kieviten en scholeksters zien we gelukkig nog wel, al is het dan nog steeds
-                de vraag
-                of ze erin slagen kuikens groot te brengen onder de huidige slechte omstandigheden in Cronesteyn.
-
-                De boomklever die in maart nog in het park rondhing, is sindsdien ook niet meer gesignaleerd.
-                Het zou ook de eerste boomklever zijn geweest die bij ons kwam broeden.
-                Met de ijsvogels ging het de laatste jaren lekker in Cronesteyn, maar dit jaar lijkt het erop dat ze
-                het
-                hebben afgelegd tegen de herhaalde vorstperiodes.
+        <div>
+            <h2>Vogelwerkgroep Cronesteyn</h2>
+            <p>Vogelwerkgroep Cronesteyn verwelkomt graag nieuwe leden! Meer informatie vind je onderaan deze
+                pagina.
             </p>
-        </div>
-        <div class="col-md-3">
-            <h3>Vogels</h3>
-            <img src="{{asset('/images/vogelfoto.jpeg')}}" alt="Foto van vogel" width="250px" height="200px">
-            <i> Information: <b> Vogel Bird</b></i><br>
-            <u> Shot date: 04-04-2018</u>
+            <h3>Waarnemingen 2018</h3>
+            <br>
+            <h3>Mei 2018</h3>
+            <p>
+                In maart zagen we nog wel wat grutto's in de weilanden, maar intussen vragen we ons af of er überhaupt
+                zijn gebleven om te broeden. Kieviten en scholeksters zien we gelukkig nog wel, al is het dan nog steeds
+                de vraag of ze erin slagen kuikens groot te brengen onder de huidige slechte omstandigheden in
+                Cronesteyn.
+                <br>
+                <br>
+                De boomklever die in maart nog in het park rondhing, is sindsdien ook niet meer gesignaleerd. Het zou
+                ook de eerste boomklever zijn geweest die bij ons kwam broeden. Met de ijsvogels ging het de laatste
+                jaren lekker in Cronesteyn, maar dit jaar lijkt het erop dat ze het hebben afgelegd tegen de herhaalde
+                vorstperiodes.
+                <br>
+                <br>
+                Gelukkig is er ook goed nieuws, bijvoorbeeld over de groene specht die we dit voorjaar al een aantal
+                keren hebben gespot (een oude bekende in Cronesteyn, maar ze zijn er niet elk jaar). De nachtegaal is
+                eerder wel eens langsgekomen en niet blijven hangen, maar dit voorjaar zit er onmiskenbaar een
+                blijvertje midden in het park en hadden we daarnaast nog een incidentele waarneming aan de parkrand. Ook
+                oude getrouwen zoals ransuilen, buizerds, sperwers en torenvalken zijn weer van de partij dit jaar,
+                ieder met één broedend paartje. De zeer bescheiden kolonie boerezwaluwen bij de Tuin van de Smid lijkt
+                te zijn uitgebreid naar zeker twee paar. Ons onervaren paartje ooievaars is er de afgelopen jaren niet
+                in geslaagd jongen groot te brengen — zal het ze dit jaar wel lukken? De voortekenen waren gunstig, ze
+                zaten braaf te broeden en maakten meer werk van het uitbouwen van het nest, maar de laatste keer was er
+                zo te zien toch niemand thuis. We zullen maar hopen dat ze gewoon diep weggedoken zaten vanwege het
+                frisse weer.
+                <br>
+                <br>
+                Dat laatste geldt eigenlijk ook voor de lepelaars, die zo'n vijf jaar geleden een kolonietje hebben
+                opgestart in gekraakte reigernesten maar die zich intussen toch weer lijken te hebben bedacht. We hebben
+                ze wel zien foerageren in de slootjes van het park, maar nog geen nesten gespot. De reigers zelf zijn
+                hun kolonie geleidelijk naar het Landgoedbos aan het verplaatsen: langzamerhand zitten verreweg de
+                meeste nesten daar in de klimop. In het Landgoedbos kun je ook de bescheiden roep van de holeduif weer
+                horen ('woe... woe...') en behalve de zang- en watervogels zijn de kauwen er lekker bezig met een
+                kolonie in de klimop.
+                <br>
+                <br>
+                Dankzij het afwisselende landschap van het polderpark — behalve echt bos zijn er ook ruigtes,
+                bosschages, weilanden en sloten met rietkragen — valt er ook dit jaar weer een heel assortiment aan
+                leuke zomervogeltjes te beluisteren: braamsluiper, bosrietzanger, rietzanger, kleine karekiet, rietgors
+                en grasmus. Laten we er nog maar even goed van genieten.
+            </p>
+            <h3>Maart 2018</h3>
+            <p>
+                Het is altijd een wat vreemde en spannende tijd in de vogelwereld, zo aan het begin van de lente. Zeker
+                als het net nog zo koud is geweest zijn de wintergasten nog massaal aanwezig, vooral smienten en grauwe
+                ganzen (gisteren ruim 240 resp. ruim 210). Aan de andere kant zitten er ook al aardig wat grauwe ganzen
+                op nesten. Gelukkig blijven ze niet allemaal in het park broeden De blauwe reigers zijn allang aan het
+                broedseizoen begonnen en andere standvogels zie je intussen ook met nestmateriaal. Een enkele kievit en
+                scholekster zijn al gearriveerd en twee grutto's gaven een show bij het landen: die blijven hier. Helaas
+                zal dat wel niet gelden voor het paartje roodborsttapuit dat zich liet bewonderen, zeldzame doortrekkers
+                die in het park even op krachten komen. De grote zilverreiger zal er ook wel binnenkort vandoorgaan. De
+                lepelaars en ooievaars zijn aan het uitzoeken wie er dit jaar de diverse nesten gaan bezetten.
+                <br>
+                <br>
+                Wat hebben we verder zoal gezien deze winter? Aardig wat krakeenden (tot 65), slobeenden (8), kuifeenden
+                (15), wintertalingen (4), smienten in wisselende aantallen tot 370, overwinterende kieviten (12), een
+                paartje sperwer, torenvalken, buizerds, de winterse meerkoetenvergadering (tot 180), twee van de
+                ijsvogels (die blijven het hele jaar en er zullen er wel een paar gesneuveld zijn in de kou), een
+                watersnip, een kramsvogel en een grote groep sijsjes.
+                <br>
+                <br>
+                Intussen zijn de voorjaarstellingen weer begonnen en is de eerste tjiftjaf vast al in het park
+                aangekomen. Wordt vervolgd!
+            </p>
+            <h3>Vogelrijkdom in het park </h3>
+            <p>
+                De leden van Vogelwerkgroep Cronesteyn tellen al sinds de opening van het park, meer dan dertig jaar
+                geleden, de vogels die er broeden. Dankzij het gevarieerde landschap van Cronesteyn zijn er aardig wat
+                soorten te bewonderen (op dit moment rond de zestig). Het weidegebied herbergt grutto's, scholeksters,
+                krakeenden en andere weidevogels
+                <br>
+                <br>
+                In de bos- en parkachtige delen zijn allerlei zangvogels te vinden, maar ook een indrukwekkende kolonie
+                blauwe reigers met rond de vijftig nesten. Sinds een paar jaar worden er ook oude reigernesten met
+                succes gekraakt door lepelaars. Dankzij de sloten en rietkragen zijn ook bijvoorbeeld de ijsvogel, de
+                rietgors en de bosrietzanger van de partij.
+            </p>
+            <h3>Het werk van de Vogelwerkgroep</h3>
+            <p>
+                VWG Cronesteyn maakt elk voorjaar een aantal rondes om alle waarnemingen in kaart te brengen die op
+                broeden
+                wijzen. Acht bezoeken worden ’s ochtends vroeg gebracht, twee in de avond (voor de uilen) en een aantal
+                overdag voor diverse eendensoorten, de reigerkolonie en de lepelaars. In de winter wordt er eens per
+                maand
+                overdag geteld om de wintergasten te inventariseren: smienten, krakeenden, diverse soorten ganzen etc.
+                Al die gegevens worden in het computersysteem van de landelijke organisatie SOVON ingevoerd; daarnaast
+                worden ze o.a. gebruikt voor het beheer van het polderpark.
+                <br>
+                <br>
+                Corinna Vermeulen (Cronesteynteller sinds 2000) vertelt: ‘Het vroege opstaan vind ik het lastigst. Maar
+                als
+                je eenmaal in het park bent en de eerste zonnestralen op de ochtendnevel ziet en al die vogels hoort,
+                dan
+                weet je weer waarom je het doet.’
+            </p>
+            <h3>Meld je aan als teller!</h3>
+            <p>
+                Wil je mee helpen tellen, dan is het wel vereist dat je algemene zangvogels als merel en zwartkop al aan
+                hun geluid kunt herkennen. De braamsluiper komt dan ter plekke wel, want je wordt natuurlijk ingewerkt
+                door een vogelteller met ervaring. We verwachten dat je een keer of acht per jaar meegaat op veldbezoek
+                (4x in de vroege ochtend en 4x overdag). Aanmelding en informatie bij Corinna Vermeulen, 071–523 18 66,
+                <a href="mailto:vwgcronesteyn@xs4all.nl">vwgcronesteyn@xs4all.nl</a>
+            </p>
         </div>
     </section>
 
